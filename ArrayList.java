a991ab6ea3320a96b017e2a4331bf5cae8b911b2
Ashish Gupta <ashishgupta.9729@gmail.com>
2020-10-07 22:32:23 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,17 +1,16 @@
 import java.util.*;  
+
 public class ArrayList{  
 public static void main(String args[]){ 
-	
-
-ArrayList list=new ArrayList();//Creating arraylist  or older jdk 
 ArrayList<String> list=new ArrayList<String>();// new method after 1.5 jdk or latest
 
-list.add("Ravi");//Adding object in arraylist  
+list.add("Ravi"); //Adding values  in arraylist  
 list.add("Vijay");    
 list.add("Ajayhgujg");  
 
 //Traversing list through Iterator  
-Iterator itr=list.iterator(); //getting the iterator 
+	
+Iterator itr= list.iterator(); //getting the iterator 
 while(itr.hasNext()) // check if iterator has the elements 
 {
 	System.out.println(itr.next()); // printing the element and move to next  
